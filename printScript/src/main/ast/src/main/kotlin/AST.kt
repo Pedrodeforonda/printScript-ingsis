2527059cc249ae5088d6502c015a8f079b5f6dc3
sonpipe0 <pipefc2003@gmail.com>
2024-08-03 23:34:56 -0300

    AST Node and Types defined

--- printScript/src/main/ast/src/main/kotlin/AST.kt
+++ printScript/src/main/ast/src/main/kotlin/AST.kt
@@ -1,9 +1,16 @@
 package org.example
 
-class AST {
+class ASTNode(private val left: ASTNode?, private val right: ASTNode?, private val value: String): Node {
 
-    AST left;
-    AST right;
-    String value;
+    override fun addLeft(node: Node): Node {
+        return ASTNode(node as ASTNode, right, value)
+    }
 
+    override fun addRight(node: Node): Node {
+        return ASTNode(left, node as ASTNode, value)
+    }
+
+    override fun addValue(value: String): Node {
+        return ASTNode(left, right, value)
+    }
 }
