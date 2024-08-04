ba1bf17cf74369d01dabc58422c6b6b3e854a161
sonpipe0 <pipefc2003@gmail.com>
2024-08-04 12:48:06 -0300

    updated folder structure

--- printScript/lexer/build.gradle.kts
+++ printScript/lexer/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
 }
 
-group = "org.example"
+group = "org.printScript"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":token"))
     testImplementation(kotlin("test"))
 }
 
