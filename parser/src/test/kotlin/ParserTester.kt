dfa46544aab24d60ad6d57d7b0431e4e5f479c23
sonpipe0 <pipefc2003@gmail.com>
2024-08-27 21:41:53 -0300

    unit tests

--- parser/src/test/kotlin/ParserTester.kt
+++ parser/src/test/kotlin/ParserTester.kt
@@ -6,6 +6,7 @@ import nodes.Identifier
 import nodes.Literal
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
 
 public class ParserTester {
     @Test
@@ -222,5 +223,60 @@ public class ParserTester {
             ),
         )
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun testErrorDeclaration() {
+        val tokens = listOf(
+            // Tokens para la primera línea: a let: number = 5;
+            Token("a", TokenType.IDENTIFIER),
+            Token("let", TokenType.LET_KEYWORD),
+            Token(":", TokenType.TYPE_ASSIGNATION),
+            Token("number", TokenType.NUMBER_TYPE),
+            Token("=", TokenType.ASSIGNATION),
+            Token("5", TokenType.NUMBER_LITERAL),
+            Token(";", TokenType.SEMICOLON),
+
+            // Tokens para la segunda línea: let b number = 4;
+            Token("let", TokenType.LET_KEYWORD),
+            Token("b", TokenType.IDENTIFIER),
+            Token("number", TokenType.NUMBER_TYPE), // Error: Falta ":"
+            Token("=", TokenType.ASSIGNATION),
+            Token("4", TokenType.NUMBER_LITERAL),
+            Token(";", TokenType.SEMICOLON),
+
+            // Tokens para la tercera línea: let c: numer = 3;
+            Token("let", TokenType.LET_KEYWORD),
+            Token("c", TokenType.IDENTIFIER),
+            Token(":", TokenType.TYPE_ASSIGNATION),
+            Token("numer", TokenType.IDENTIFIER), // Error: Typo en "number"
+            Token("=", TokenType.ASSIGNATION),
+            Token("3", TokenType.NUMBER_LITERAL),
+            Token(";", TokenType.SEMICOLON),
+
+            // Tokens para la cuarta línea: a = 3 + 84 + "hey";
+            Token("a", TokenType.IDENTIFIER),
+            Token("=", TokenType.ASSIGNATION),
+            Token("3", TokenType.NUMBER_LITERAL),
+            Token("+", TokenType.PLUS),
+            Token("84", TokenType.NUMBER_LITERAL),
+            Token("+", TokenType.PLUS),
+            Token("hey", TokenType.STRING_LITERAL), // Error: Asignación de número a string
+            Token(";", TokenType.SEMICOLON),
+
+            // Tokens para la quinta línea: println("Result: " + a);
+            Token("println", TokenType.CALL_FUNC),
+            Token("(", TokenType.LEFT_PAREN),
+            Token("Result:", TokenType.STRING_LITERAL),
+            Token("+", TokenType.PLUS),
+            Token("a", TokenType.IDENTIFIER),
+            Token(")", TokenType.RIGHT_PAREN),
+            Token(";", TokenType.SEMICOLON),
+        )
+
+        val parser = Parser(tokens)
+        assertFailsWith<ParseException> {
+            parser.parseExpressions()
+        }
     }
 }
