5ec826db0ef9115f8dc7eceb557d23c5d3fa2fce
sonpipe0 <pipefc2003@gmail.com>
2024-08-28 16:35:16 -0300

    demo formatter. Problema con la lectura del json

--- formatter/build.gradle.kts
+++ formatter/build.gradle.kts
@@ -8,4 +8,5 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
 }
---
+++ formatter/src/main/kotlin/FormatterConfigReader.kt
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
+
+data class FormatterConfigReader(
+    val spaceBeforeColon: Boolean,
+    val spaceAfterColon: Boolean,
+    val spaceBeforeAssignment: Boolean,
+    val spaceAfterAssignment: Boolean,
+    val newLinesBeforePrintln: Int,
+)
+
+fun loadConfig(configPath: String): FormatterConfigReader {
+    val mapper = jacksonObjectMapper()
+    val file = File(configPath)
+    return mapper.readValue(file)
+}
