00ffea865b5b60c2162e1667c701638313d42c70
sonpipe0 <pipefc2003@gmail.com>
2024-08-29 15:15:13 -0300

    cd job created

---
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-publish-package-conventions.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "com.github.jitpack"
+version = "2.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:29.0-jre")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/printSrcript/master")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+// Correct way to configure the wrapper task in a convention script
+tasks.named<Wrapper>("wrapper") {
+    gradleVersion = "7.3.3"
+    distributionType = Wrapper.DistributionType.ALL
+}
