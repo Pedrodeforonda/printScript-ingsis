9ff5d2f335d122e6222851bc78cca76cf006be5e
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2024-09-02 00:07:43 -0300

    cli formatter

--- cli/src/main/kotlin/cli/FormatterOperation.kt
+++ cli/src/main/kotlin/cli/FormatterOperation.kt
@@ -1,8 +1,11 @@
 package cli
 
+import FormatterConfigReader
+import MainFormatter
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
+import loadConfig
 import java.io.File
 
 class FormatterOperation : CliktCommand(
@@ -12,6 +15,14 @@ class FormatterOperation : CliktCommand(
     private val sourceFile: File by argument(help = "Source file to process.")
         .file(mustExist = true)
 
+    private val configFile: File by argument(help = "Config file to use.")
+        .file(mustExist = true)
+
     override fun run() {
+        val outputPath = "src/main/resources/formattedCode.txt"
+        val config = loadConfig<FormatterConfigReader>(configFile.absolutePath)
+        val formattedText = MainFormatter().formatCode(sourceFile.readText(), config)
+        File(outputPath).writeText(formattedText)
+        println("Formatted code written to $outputPath")
     }
 }
