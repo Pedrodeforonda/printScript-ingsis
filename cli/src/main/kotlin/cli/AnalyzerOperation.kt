a8fd666a6d4e41d5504f36e8a9be71db554be568
sonpipe0 <pipefc2003@gmail.com>
2024-09-01 23:20:27 -0300

    fixed tests

--- cli/src/main/kotlin/cli/AnalyzerOperation.kt
+++ cli/src/main/kotlin/cli/AnalyzerOperation.kt
@@ -5,7 +5,8 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
 import main.kotlin.ConfigParser
 import main.kotlin.Linter
-import main.kotlin.strategies.ClassicLinterStrategies
+import main.kotlin.strategies.CamelStrategy
+import main.kotlin.strategies.SnakeStrategy
 import java.io.File
 
 class AnalyzerOperation : CliktCommand(
@@ -19,8 +20,9 @@ class AnalyzerOperation : CliktCommand(
         .file(mustExist = true)
 
     override fun run() {
+        val linterStrategies = mapOf("camelCase" to CamelStrategy(), "snake_case" to SnakeStrategy())
         val config = ConfigParser.parseConfig(configFile.absolutePath)
-        val linter = Linter(config, ClassicLinterStrategies())
+        val linter = Linter(config, linterStrategies)
         val errors = linter.lintFile(sourceFile)
         if (errors.isEmpty()) {
             println("No errors found.")
