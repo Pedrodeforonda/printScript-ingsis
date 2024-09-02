91a1ba792466704dbb3cd7d2d48c2f8cd88f479d
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2024-09-02 14:58:02 -0300

    lazy interpreter

--- ast/src/main/kotlin/Result.kt
+++ ast/src/main/kotlin/Result.kt
@@ -8,4 +8,4 @@ data class LiteralResult(val value: Any) : Result
 
 data class SuccessResult(val message: String) : Result
 
-data class FailureResult(val message: String) : Result
+data class InterpreterException(val message: String) : Result
