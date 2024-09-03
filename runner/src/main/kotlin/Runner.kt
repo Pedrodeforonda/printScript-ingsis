8743842ca0f54b62473bbeeb58f67873c58e560e
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2024-09-03 14:22:07 -0300

    lexer-lazy

--- runner/src/main/kotlin/Runner.kt
+++ runner/src/main/kotlin/Runner.kt
@@ -2,6 +2,7 @@ package org.example
 
 import Parser
 import Token
+import java.io.BufferedReader
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -9,9 +10,11 @@ class Runner {
 
     fun run(path: String) {
         val byteArr: ByteArray = Files.readAllBytes(Paths.get(path))
-        val lexer = Lexer(byteArr.toString(Charsets.UTF_8), ClassicTokenStrategies())
-        val tokens: List<Token> = lexer.tokenizeAll(lexer)
-        val parser = Parser(tokens.listIterator())
+        val text: String = byteArr.toString(Charsets.UTF_8)
+        val bufferedReader: BufferedReader = text.reader().buffered()
+        val lexer = Lexer(bufferedReader)
+        val tokens: List<Token> = lexer.tokenizeAll(lexer).toList()
+        val parser = Parser(tokens)
         val ast = parser.parseExpressions()
         val interpreter = Interpreter()
         interpreter.interpret(ast)
