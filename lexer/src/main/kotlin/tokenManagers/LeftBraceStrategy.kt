a2e55f162bf711b2af11f3ecb6115dbf02afdfa0
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-09-05 15:20:08 -0300

    Brace Strategies

--- lexer/src/main/kotlin/tokenManagers/LeftBraceStrategy.kt
+++ lexer/src/main/kotlin/tokenManagers/LeftBraceStrategy.kt
@@ -6,7 +6,7 @@ import main.TokenType
 import org.example.lexer.Lexer
 import org.example.lexer.TokenStrategy
 
-class LeftBraceStrategy: TokenStrategy {
+class LeftBraceStrategy : TokenStrategy {
     override fun buildToken(lexer: Lexer, result: String, initialPosition: Position): Token? {
         if (lexer.getChar() == '{') {
             return Token("{", TokenType.LEFT_BRACE, initialPosition)
--- lexer/src/main/kotlin/tokenManagers/RightBraceStrategy.kt
+++ lexer/src/main/kotlin/tokenManagers/RightBraceStrategy.kt
@@ -6,7 +6,7 @@ import main.TokenType
 import org.example.lexer.Lexer
 import org.example.lexer.TokenStrategy
 
-class RightBraceStrategy: TokenStrategy {
+class RightBraceStrategy : TokenStrategy {
     override fun buildToken(lexer: Lexer, result: String, initialPosition: Position): Token? {
         if (lexer.getChar() == '}') {
             return Token("}", TokenType.RIGHT_BRACE, initialPosition)
