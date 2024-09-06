a17eab99ab33907987bdb95edd779bec80cde350
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2024-09-05 22:29:49 -0300

    linter config fix

--- linter/src/main/kotlin/main/LinterConfig.kt
+++ linter/src/main/kotlin/main/LinterConfig.kt
@@ -1,3 +1,8 @@
 package main.kotlin.main
 
-data class LinterConfig(val identifierFormat: String, val restrictPrintln: Boolean)
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class LinterConfig(
+    var identifier_format: String = "",
+    @JsonProperty("mandatory-variable-or-literal-in-println") var restrictPrintln: Boolean = false
+)
