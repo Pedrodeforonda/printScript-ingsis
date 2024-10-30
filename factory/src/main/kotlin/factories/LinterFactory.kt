5e732c9de678f64d48bdd86a1a24642901803e59
sonpipe0 <pipefc2003@gmail.com>
2024-10-30 14:25:47 -0300

    new linter config receival

--- factory/src/main/kotlin/factories/LinterFactory.kt
+++ factory/src/main/kotlin/factories/LinterFactory.kt
@@ -3,6 +3,7 @@ package factories
 import dataObjects.LinterResult
 import main.ConfigParser
 import main.Linter
+import main.LinterConfig
 import utils.PercentageCollector
 import java.io.InputStream
 
@@ -13,5 +14,12 @@ class LinterFactory {
         val linterConfig = ConfigParser.parseConfigToMap(config)
         val nodes = ValidatorFactory().validate(src, version, collector)
         return Linter().lint(nodes, linterConfig)
+    }
+
+    fun lintCode(src: InputStream, version: String, config: LinterConfig):
+        Sequence<LinterResult> {
+        val collector = PercentageCollector()
+        val nodes = ValidatorFactory().validate(src, version, collector)
+        return Linter().lint(nodes, config)
     }
 }
